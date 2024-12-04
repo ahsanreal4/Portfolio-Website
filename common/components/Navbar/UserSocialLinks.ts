ec64666dfc43e8cd974df3b3d7be8609fe2402e7
ahsanreal4 <ahsan.btph123@gmail.com>
2024-12-04 16:44:29 +0500

    add social links and fix errors

--- common/components/Navbar/UserSocialLinks.ts
+++ common/components/Navbar/UserSocialLinks.ts
@@ -8,17 +8,17 @@ export const USER_SOCIAL_LINKS: UserSocialLink[] = [
   },
   {
     linkIconClassName: "fab fa-linkedin-in fa-fw",
-    linkUrl: "#",
+    linkUrl: "https://www.linkedin.com/in/ahsan-azeem-41b462190/",
     userName: "username",
   },
   {
     linkIconClassName: "fab fa-github-alt fa-fw",
-    linkUrl: "#",
+    linkUrl: "https://github.com/ahsanreal4",
     userName: "username",
   },
   {
     linkIconClassName: "fab fa-stack-overflow fa-fw",
-    linkUrl: "#",
+    linkUrl: "https://stackoverflow.com/users/28624113/ahsan-azeem",
     userName: "username",
   },
   {
