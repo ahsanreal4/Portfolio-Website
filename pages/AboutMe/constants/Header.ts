25c98d529ed87aaaa23e93e7e89192918ff24d15
ahsanreal4 <ahsan.btph123@gmail.com>
2024-12-04 16:46:45 +0500

    fix error

--- pages/AboutMe/constants/Header.ts
+++ pages/AboutMe/constants/Header.ts
@@ -1,8 +1,12 @@
-import { IAboutMeHeaderPros } from '../components/Header/Header.types'
+type IHeaderProps = {
+  name: string;
+  introductionText: string;
+  position: string;
+};
 
-export const HeaderProps: IAboutMeHeaderPros = {
+export const HeaderProps: IHeaderProps = {
   introductionText:
-    'I have been passionate about programming since I was 13 and I loveto learn. I believe in the learn by doing approach because I have always learned from mistakes. I love my job so much that I do not seem to be working. Sometimes I take decisions, sometimes I change my mind, but everytime I get to the best solution.',
-  name: 'Riccardo Sacco',
-  position: 'Full Stack Developer'
-}
+    "I have been passionate about programming since I was 13 and I loveto learn. I believe in the learn by doing approach because I have always learned from mistakes. I love my job so much that I do not seem to be working. Sometimes I take decisions, sometimes I change my mind, but everytime I get to the best solution.",
+  name: "Riccardo Sacco",
+  position: "Full Stack Developer",
+};
